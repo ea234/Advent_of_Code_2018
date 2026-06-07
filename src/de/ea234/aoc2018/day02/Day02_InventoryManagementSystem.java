52a674a388eb286ee638a84edc1471be40d50a34
ea234 <anyang@gmx.de>
2026-06-07 11:39:29 +0200

    Day 02 - Part 2

--- src/de/ea234/aoc2018/day02/Day02_InventoryManagementSystem.java
+++ src/de/ea234/aoc2018/day02/Day02_InventoryManagementSystem.java
@@ -15,7 +15,9 @@ import java.util.stream.Collectors;
  * 
  * https://www.reddit.com/r/adventofcode/comments/a2aimr/2018_day_2_solutions/
  * 
+ * 
  * ------------------------------------------------------------------------------------------
+ * 
  * abcdef  Two Times 0   Three Times 0 
  * bababc  Two Times 1   Three Times 1 
  * abbcde  Two Times 1   Three Times 0 
@@ -27,16 +29,41 @@ import java.util.stream.Collectors;
  * count_2_times_res 4
  * count_3_times_res 3
  * 
+ * input_a abcdee
+ * input_b abcdef
+ * 
  * Result Part 1 12
- * Result Part 2 0
+ * Result Part 2 abcde
+ * 
+ * ------------------------------------------------------------------------------------------
+ * 
+ * abcde  Two Times 0   Three Times 0 
+ * fghij  Two Times 0   Three Times 0 
+ * klmno  Two Times 0   Three Times 0 
+ * pqrst  Two Times 0   Three Times 0 
+ * fguij  Two Times 0   Three Times 0 
+ * axcye  Two Times 0   Three Times 0 
+ * wvxyz  Two Times 0   Three Times 0 
+ * 
+ * count_2_times_res 0
+ * count_3_times_res 0
+ * 
+ * input_a fghij
+ * input_b fguij
+ * 
+ * Result Part 1 0
+ * Result Part 2 fgij
  * 
  * ------------------------------------------------------------------------------------------
  * 
  * count_2_times_res 248
  * count_3_times_res 31
  * 
+ * input_a lsrivmotzbdxpkxnaqmuwcgchj
+ * input_b lsrivmotzbdxpkxnaqmuwcychj
+ * 
  * Result Part 1 7688
- * Result Part 2 0
+ * Result Part 2 lsrivmotzbdxpkxnaqmuw
  * 
  * </pre> 
  */
@@ -44,9 +71,10 @@ public class Day02_InventoryManagementSystem
 {
   public static void main( String[] args )
   {
-    String test_input = "abcdef,bababc,abbcde,abcccd,aabcdd,abcdee,ababab";
 
-    calculatePart01( test_input, true );
+    calculatePart01( "abcdef,bababc,abbcde,abcccd,aabcdd,abcdee,ababab", true );
+
+    calculatePart01( "abcde,fghij,klmno,pqrst,fguij,axcye,wvxyz",        true );
 
     calculate01( getListProd(), false );
 
@@ -67,7 +95,7 @@ public class Day02_InventoryManagementSystem
 
     long result_part_01 = 0;
 
-    long result_part_02 = 0;
+    String result_part_02 = "";
 
     int count_2_times_res = 0;
 
@@ -75,8 +103,14 @@ public class Day02_InventoryManagementSystem
 
     for ( String input_str : pListInput )
     {
+      /*
+       * Create a vector for character add ups
+       */
       int[] ascii_count = new int[ 30 ];
 
+      /*
+       * Iterate over the input string and add up the char-frequenzies
+       */
       for ( int idx = 0; idx < input_str.length(); idx++ )
       {
         ascii_count[ ( (int) input_str.charAt( idx ) ) - 97 ]++;
@@ -105,13 +139,70 @@ public class Day02_InventoryManagementSystem
 
     result_part_01 = count_2_times_res * count_3_times_res;
 
+    pListInput.sort( null );
+
+    int error_count = 0;
+
+    String input_a = null;
+    String input_b = null;
+
+    for ( int index_input = 1; index_input < pListInput.size(); index_input++ )
+    {
+      input_a = pListInput.get( index_input - 1 );
+
+      input_b = pListInput.get( index_input );
+
+      error_count = 0;
+
+      for ( int idx = 0; idx < input_a.length(); idx++ )
+      {
+        if ( input_a.charAt( idx ) == input_b.charAt( idx ) )
+        {
+          // OK
+        }
+        else
+        {
+          /*
+           * Increase the error count.
+           */
+          error_count++;
+
+          /*
+           * If the error count is 2, the check of the 2 strings is over.
+           */
+          if ( error_count == 2 )
+          {
+            break;
+          }
+        }
+      }
+
+      /*
+       * If there is only 1 error count, the result strings are found
+       */
+      if ( error_count == 1 )
+      {
+        break;
+      }
+    }
+
+    for ( int idx = 0; idx < input_a.length(); idx++ )
+    {
+      if ( input_a.charAt( idx ) == input_b.charAt( idx ) )
+      {
+        result_part_02 += "" + input_a.charAt( idx );
+      }
+    }
+
     wl( "" );
     wl( "count_2_times_res " + count_2_times_res );
     wl( "count_3_times_res " + count_3_times_res );
     wl( "" );
+    wl( "input_a " + input_a );
+    wl( "input_b " + input_b );
+    wl( "" );
     wl( "Result Part 1 " + result_part_01 );
     wl( "Result Part 2 " + result_part_02 );
-    wl( "" );
     wl( "" );
   }
 
